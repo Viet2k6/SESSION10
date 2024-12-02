bedc2e8a638971e3a240c5c27afc538c9f885964
Đăng Việt <trandangviet00@gmail.com>
2024-12-02 22:55:39 +0700

    Add files via upload

---
+++ ss10-b2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int i,j,temp;
+	int arr[]={5,22,13,7,41,8};
+    int size=sizeof(arr)/sizeof(int);
+    for(i=0;i<size;i++){
+    	for(j=0;j<size-i-1;j++){
+    		if(arr[j+1]<arr[j]){
+    			temp=arr[j];
+    			arr[j]=arr[j+1];
+    			arr[j+1]=temp;
+    		}
+		}
+    }
+	for(i=0;i<size;i++){
+		printf("%d\t",arr[i]);
+	}
+	return 0;
+}
